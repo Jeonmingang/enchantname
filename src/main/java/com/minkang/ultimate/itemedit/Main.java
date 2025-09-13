3d8277643e4a54174582228391c7e7affb57a208
SAMKOREA <123339750+Jeonmingang@users.noreply.github.com>
2025-09-13 20:35:20 +0900

    Add files via upload

--- src/main/java/com/minkang/ultimate/itemedit/Main.java
+++ src/main/java/com/minkang/ultimate/itemedit/Main.java
@@ -2,39 +2,16 @@ package com.minkang.ultimate.itemedit;
 
 import com.minkang.ultimate.itemedit.commands.ItemLoreCommand;
 import com.minkang.ultimate.itemedit.commands.ItemNameCommand;
-import com.minkang.ultimate.itemedit.commands.EnchantCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
-
     @Override
     public void onEnable() {
-        // Register commands
-        if (getCommand("아이템이름") != null) {
-            getCommand("아이템이름").setExecutor(new ItemNameCommand());
-        }
-        if (getCommand("아이템설명") != null) {
-            getCommand("아이템설명").setExecutor(new ItemLoreCommand());
-        }
-if (getCommand("인첸트") != null) {
-    EnchantCommand ec = new EnchantCommand(this);
-    getCommand("인첸트").setExecutor(ec);
-    getCommand("인첸트").setTabCompleter(ec);
-}
-if (getCommand("인첸트리로드") != null) {
-    getCommand("인첸트리로드").setExecutor((sender, cmd, lbl, args) -> {
-        if (sender instanceof org.bukkit.entity.Player) {
-            org.bukkit.entity.Player p = (org.bukkit.entity.Player) sender;
-            if (!p.hasPermission("ultimate.itemedit.admin")) {
-                p.sendMessage(getConfig().getString("no-perm", "§c권한이 없습니다."));
-                return true;
-            }
-        }
-        reloadConfig();
-        sender.sendMessage(getConfig().getString("prefix", "") + "§a설정을 리로드했습니다.");
-        return true;
-    });
-}
+        if (getCommand("아이템이름") != null) getCommand("아이템이름").setExecutor(new ItemNameCommand());
+        if (getCommand("아이템설명") != null) getCommand("아이템설명").setExecutor(new ItemLoreCommand());
+        // ASCII aliases for CatServer edge cases
+        if (getCommand("iname") != null) getCommand("iname").setExecutor(new ItemNameCommand());
+        if (getCommand("ilore") != null) getCommand("ilore").setExecutor(new ItemLoreCommand());
         getLogger().info("[UltimateItemEdit] Enabled v" + getDescription().getVersion());
     }
 
--- src/main/java/com/minkang/ultimate/itemedit/commands/ItemNameCommand.java
+++ src/main/java/com/minkang/ultimate/itemedit/commands/ItemNameCommand.java
@@ -10,60 +10,26 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
 public class ItemNameCommand implements CommandExecutor {
-
-    private String colorize(String s) {
-        return ChatColor.translateAlternateColorCodes('&', s);
-    }
+    private String c(String s){ return ChatColor.translateAlternateColorCodes('&', s); }
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (!(sender instanceof Player)) {
-            sender.sendMessage("플레이어만 사용 가능합니다.");
-            return true;
+        if (!(sender instanceof Player)) { sender.sendMessage("플레이어만 사용 가능합니다."); return true; }
+        Player p=(Player)sender;
+        if (!p.isOp() && !p.hasPermission("ultimate.itemedit.name")) { p.sendMessage(c("&c권한이 없습니다.")); return true; }
+        ItemStack hand=p.getInventory().getItemInMainHand();
+        if (hand==null || hand.getType()==Material.AIR){ p.sendMessage(c("&e손에 든 아이템이 없습니다.")); return true; }
+        if (args.length==0){ p.sendMessage(c("&b사용법: &f/아이템이름 <이름...> &7또는 &f/아이템이름 삭제 (&7/iname 별칭 지원&f)")); return true; }
+        ItemMeta meta=hand.getItemMeta();
+        if (meta==null){ p.sendMessage(c("&c이 아이템은 이름을 변경할 수 없습니다.")); return true; }
+        if (args[0].equalsIgnoreCase("삭제")){
+            meta.setDisplayName(null); hand.setItemMeta(meta); p.sendMessage(c("&a아이템 이름을 제거했습니다.")); return true;
         }
-        Player p = (Player) sender;
-
-        if (!p.isOp() && !p.hasPermission("ultimate.itemedit.name")) {
-            p.sendMessage(colorize("&c권한이 없습니다."));
-            return true;
-        }
-
-        ItemStack hand = p.getInventory().getItemInMainHand();
-        if (hand == null || hand.getType() == Material.AIR) {
-            p.sendMessage(colorize("&e손에 든 아이템이 없습니다."));
-            return true;
-        }
-
-        if (args.length == 0) {
-            p.sendMessage(colorize("&b사용법: &f/아이템이름 <이름...> &7또는 &f/아이템이름 삭제"));
-            return true;
-        }
-
-        String sub = args[0];
-
-        ItemMeta meta = hand.getItemMeta();
-        if (meta == null) {
-            p.sendMessage(colorize("&c이 아이템은 이름을 변경할 수 없습니다."));
-            return true;
-        }
-
-        if (sub.equalsIgnoreCase("삭제")) {
-            meta.setDisplayName(null);
-            hand.setItemMeta(meta);
-            p.sendMessage(colorize("&a아이템 이름을 제거했습니다."));
-            return true;
-        }
-
-        // Treat whole args as name
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < args.length; i++) {
-            if (i > 0) sb.append(" ");
-            sb.append(args[i]);
-        }
-        String newName = colorize(sb.toString());
-        meta.setDisplayName(newName);
-        hand.setItemMeta(meta);
-        p.sendMessage(colorize("&a아이템 이름을 설정했습니다: &r") + newName);
+        StringBuilder sb=new StringBuilder();
+        for(int i=0;i<args.length;i++){ if(i>0) sb.append(" "); sb.append(args[i]);}
+        String newName=c(sb.toString());
+        meta.setDisplayName(newName); hand.setItemMeta(meta);
+        p.sendMessage(c("&a아이템 이름을 설정했습니다: &r")+newName);
         return true;
     }
 }
